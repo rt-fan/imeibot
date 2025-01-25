9c6294d259c8e1b1517b1d14d81671de8911c10e
rt_fan <rt_fan@mail.ru>
2025-01-25 09:59:56 +0000

    upd

--- bot/bot.py
+++ bot/bot.py
@@ -1,44 +1,22 @@
-# bot/bot.py
-
 import os
 import logging
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-import httpx
-import re
 from dotenv import load_dotenv
+# from imei_check_api import is_valid_imei, check_api
+from imei_api.imei_check_api import is_valid_imei, check_api
 
 load_dotenv()
 
-# Получаем токен бота и белый список пользователей из переменных окружения
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 WHITELIST_USER_IDS = set(map(int, os.getenv('WHITELIST_USER_IDS').split(',')))
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dp = Dispatcher()
 
-def is_valid_imei(imei: str) -> bool:
-    return bool(re.match(r'^\d{15}$', imei))
-
-async def query_imeicheck(imei: str) -> dict:
-    api_url = "https://imeicheck.net/ajax/promo-check"
-    headers = {
-        "Content-Type": "application/json",
-    }
-    payload = {
-        "api_key": "e4oEaZY1Kom5OXzybETkMlwjOCy3i8GSCGTHzWrhd4dc563b",  # Sandbox API Key
-        "imei": imei
-    }
-    async with httpx.AsyncClient() as client:
-        response = await client.post(api_url, json=payload, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return None
 
 @dp.message()
 async def handle_message(message: Message):
@@ -52,7 +30,7 @@ async def handle_message(message: Message):
         await message.reply("Пожалуйста, введите корректный 15-значный IMEI.")
         return
     
-    imei_info = await query_imeicheck(imei)
+    imei_info = await check_api(imei)
     if imei_info is None:
         await message.reply("Не удалось получить информацию об IMEI.")
         return
@@ -61,11 +39,13 @@ async def handle_message(message: Message):
     response_text = f"Информация об IMEI:\n\n{imei_info}"
     await message.reply(response_text)
 
+
 async def main():
     try:
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
--- imei_api/imei_check_api.py
+++ imei_api/imei_check_api.py
@@ -1,34 +1,28 @@
-# Required Libraries
 import requests
 import json
 import os
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-# Получаем токен из переменных окружения
 IMEI_API_TOKEN = os.getenv('IMEI_API_TOKEN')
-# Base URL
 url = 'https://api.imeicheck.net/v1/checks'
 
 
-def check_api(imei: str):
-    # Add necessary headers
+def is_valid_imei(imei: str) -> bool:
+    return bool(re.match(r'^\d{15}$', imei))
+
+
+async def check_api(imei: str):
     headers = {
     'Authorization': 'Bearer ' + IMEI_API_TOKEN,
     'Content-Type': 'application/json'
     }
-
-    # Add body
     body =  json.dumps({
     "deviceId": imei,
     "serviceId": 1
     })
-
-    # Execute request
     response = requests.post(url, headers=headers, data=body)
-
     return response.text
-
-print(check_api("356735111052198"))
